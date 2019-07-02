f12f1d7829121c80ab17fd93f3d20d48c03efaf4
itsSentio <rs.smiffershd@gmail.com>
2019-07-02 13:42:10 -0400

    tset

--- listener.js
+++ listener.js
@@ -9,6 +9,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const strengthMessage = document.getElementById('passwordstrengthvalidation')
   const submitButton = document.getElementById('submitbutton')
 
+  var validEmail = false;
+  var passwordStrength = false;
+  var passwordMatches = false;
+
+  function submitReady() {
+    if (validEmail && passwordStrength && passwordMatches) {
+      submitButton.disabled = false;
+    }
+  }
+
   function strengthCheck(password) {
         var regExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&*()]).{8,}/;
         var validPassword = regExp.test(password);
@@ -17,38 +27,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   emailField.addEventListener('keyup', function(event) {
     isValidEmail = emailField.checkValidity();
+    submitReady()
     if (isValidEmail) {
       emailMessage.style.display = "none";
       emailField.style.borderColor = "black";
+      validEmail = true;
     } else {
       emailMessage.style.display = "block";
       emailField.style.borderColor = "red";
+      validEmail = false;
     }
   });
 
   passField.addEventListener('keyup', function(event) {
+    submitReady()
     if (strengthCheck(passField.value)) {
       strengthMessage.style.display = "none";
+      passwordStrength = true;
     } else {
       strengthMessage.style.display = "block";
+      passwordStrength = false;
     }
 
     if (passConField.value == passField.value) {
       passMessage.style.display = "none";
       passConField.style.borderColor = 'black';
+      passwordMatches = true;
     } else if (passConField.value != "") {
       passMessage.style.display = "block";
       passConField.style.borderColor = 'red';
+      passwordMatches = false;
     }
   });
 
   passConField.addEventListener('keyup', function(event) {
+    submitReady()
     if (passConField.value == passField.value) {
       passMessage.style.display = "none";
       passConField.style.borderColor = 'black';
+      passwordMatches = true;
     } else {
       passMessage.style.display = "block";
       passConField.style.borderColor = 'red';
+      passwordMatches = false;
     }
   });
 });
