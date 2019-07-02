013f6b41890aa2dbda5ddbc9062ca1205d7420d6
itsSentio <rs.smiffershd@gmail.com>
2019-07-02 01:58:55 -0400

    test

--- listener.js
+++ listener.js
@@ -4,12 +4,14 @@ const passField = document.getElementById('password');
 const passConField = document.getElementById('confirmpassword');
 const passMatch = document.getElementById('passmatch')
 
-passConField.addEventListener('keyup', function(event) {
-  if (passConField.value == passField.value) {
-    passMatch.style.display = "none";
-    passConField.style.borderColor = 'black';
-  } else {
-    passMatch.style.display = "block";
-    passConField.style.borderColor = 'red';
-  }
+document.addEventListener("DOMContentLoaded", function(event) {
+  passConField.addEventListener('keyup', function(event) {
+    if (passConField.value == passField.value) {
+      passMatch.style.display = "none";
+      passConField.style.borderColor = 'black';
+    } else {
+      passMatch.style.display = "block";
+      passConField.style.borderColor = 'red';
+    }
+  });
 });
