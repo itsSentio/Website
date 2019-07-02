313d826158fd4bec311d1ed8886e522ac38c6ee6
itsSentio <rs.smiffershd@gmail.com>
2019-07-02 14:01:17 -0400

    updated password fields

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
          Email:<br>
          <input type="email" name="email" id="emailfield"><br>
          Password:<br>
-         <input type="text" name="password" id="passwordfield"><br>
+         <input type="password" name="password" id="passwordfield"><br>
          Confirm Password:<br>
-         <input type="text" name="confirmpassword" id="confirmpasswordfield"><br><br>
+         <input type="password" name="confirmpassword" id="confirmpasswordfield"><br><br>
          <input type="submit" value="Register" id="submitbutton" disabled>
          <p id="passwordvalidation">Passwords must match</p>
          <p id="emailvalidation">Must be a valid email address</p>
