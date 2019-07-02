7cd005abce575bf82fc60f345161a9cf4e30e6b2
itsSentio <rs.smiffershd@gmail.com>
2019-07-02 02:02:33 -0400

    test

--- listener.js
+++ listener.js
@@ -1,7 +1,7 @@
 const registerForm = document.getElementById('registerForm');
-const emailField = document.getElementByName('email');
-const passField = document.getElementByName('password');
-const passConField = document.getElementByName('confirmpassword');
+const emailField = document.getElemenstByName('email');
+const passField = document.getElementsByName('password');
+const passConField = document.getElementsByName('confirmpassword');
 const passMatch = document.getElementById('passmatch')
 
 document.addEventListener("DOMContentLoaded", function(event) {
