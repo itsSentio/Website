3bd8e3cc05707a67ae9e5ec495c4b39af40166f2
itsSentio <rs.smiffershd@gmail.com>
2019-07-02 02:04:48 -0400

    test

--- listener.js
+++ listener.js
@@ -1,10 +1,11 @@
-const registerForm = document.getElementById('registerForm');
-const emailField = document.getElementsByName('email');
-const passField = document.getElementsByName('password');
-const passConField = document.getElementsByName('confirmpassword');
-const passMatch = document.getElementById('passmatch')
-
 document.addEventListener("DOMContentLoaded", function(event) {
+
+  const registerForm = document.getElementById('registerForm');
+  const emailField = document.getElementsByName('email');
+  const passField = document.getElementsByName('password');
+  const passConField = document.getElementsByName('confirmpassword');
+  const passMatch = document.getElementById('passmatch')
+  
   passConField.addEventListener('keyup', function(event) {
     if (passConField.value == passField.value) {
       passMatch.style.display = "none";
